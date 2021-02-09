7399594634c9ccc2bb7c09fe58c4890d53fc5751
WarlockGames12 <fpweeyou@gmail.com>
2021-02-09 15:05:00 +0100

    somebody

---
+++ Les1CS-BO/Les1CS-BO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Les1CS_BO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Set the Foreground color to green
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Display current Foreground color 
+            Console.WriteLine("Hello World!");
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
